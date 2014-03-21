56db0b27f322f3c694b0deb7df64cfc32e9dcf37
johnshanley <john.michael.shanley@gmail.com>
2014-03-21 13:10:38 -0400

    add Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    nodeunit: {
+      all: ['test/**/*_test.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  grunt.registerTask('default', ['nodeunit']);
+  grunt.registerTask('test', ['nodeunit']);
+};
