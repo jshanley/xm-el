80d0a4b8f1bc73b3c89ef8be78fc90fc88cbde12
johnshanley <john.michael.shanley@gmail.com>
2014-06-21 13:17:37 -0400

    include test of object passing to .attr()

--- test/element_test.js
+++ test/element_test.js
@@ -1,7 +1,7 @@
 var xml = require('../');
 
 exports.element = function(test) {
-  test.expect(6);
+  test.expect(8);
   test.equal(xml.element('test').stringify(), '<test></test>');
   test.equal(xml.element('br', true).stringify(), '<br />');
   var p = xml.element('parent').attr('name', 'Bob');
@@ -10,5 +10,11 @@ exports.element = function(test) {
   test.equal(c._value, 'child text');
   test.equal(c.text(), c._value);
   test.equal(p.attributes.name, 'Bob');
+  var el = xml.element('elem').attr({
+    'foo': 6,
+    bar: 'baz'
+  });
+  test.equal(el.attributes.foo, 6);
+  test.equal(el.attributes.bar, 'baz');
   test.done();
 };
